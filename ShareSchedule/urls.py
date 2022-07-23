5c2cb750b6e1f24fdae4700a63ee9c395c26b225
lsw725 <725lsw@hanmail.net>
2022-07-23 12:39:59 +0900

    add apps

--- ShareSchedule/urls.py
+++ ShareSchedule/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from empty_time import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
 ]
