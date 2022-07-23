d46bcb09443b5ad6a454f2feee60bc27d05dc02e
yoonezi <jeji3391@naver.com>
2022-07-23 16:08:08 +0900

    home.html과 css 수정

---
+++ staticfiles/js/home.js
@@ -0,0 +1,26 @@
+window.onload = function(){    
+    let class_add = document.getElementsByClassName('add-class-btn');
+    class_add[0].addEventListener('click', function(event){
+        popup();
+    });
+
+    let class_add_complete_btn = document.getElementById("complete-add");
+    class_add_complete_btn.addEventListener('click',function(event){
+        popdown();
+    });
+
+    let add_dialog = document.getElementById("class-adding");
+    add_dialog.style.visibility = "hidden";
+};
+
+function popup(){
+    console.log("popup")
+    let add_dialog = document.getElementById("class-adding");
+    add_dialog.style.visibility = "visible";
+}
+
+function popdown(){
+    console.log("popdown")
+    let add_dialog = document.getElementById("class-adding");
+    add_dialog.style.visibility = "hidden";
+}
